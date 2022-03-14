64d81ac3fae45efc32977ecf083cce31ba69194e
Oyensange <98943534+Oyensange@users.noreply.github.com>
2022-03-14 11:18:38 +0700

    Create donasi2.js

---
+++ plugins/donasi2.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
+╭╡
+│┝‷✧ *Pulsa Tree:* 0896380744555/089636249477
+│┝‷✧ *Dana:* 089636249477
+│┝‷✧ *Gopay:* 089638074455
+│╰───···─────
+│⁺◛˖ Ingin Donasi? Chat
+│wa.me/6281230926688?text=kak+mau+donasi+ni:)
+╰──────────···───╮
+▌│█║▌║▌║║▌║▌║█│▌
+`.trim(), `Kocheng`, 'Owner', '.owner')
+
+module.exports = handler
