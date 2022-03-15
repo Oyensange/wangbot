2ab49e9a7053ccce2fbb3240f30f941bdc96547a
Oyensange <98943534+Oyensange@users.noreply.github.com>
2022-03-15 12:17:05 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,7 +6,7 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/skatch?apikey=APIKEY&url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbmrYfgiRt1lLwNgRMgMbID62qH3EQ2T1c6RUJEQMcoCWKqVVxAg7R_LfM&s=10`)
+        let let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
         if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
